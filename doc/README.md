d55725c6b22e4e8ee936c59e83c2d3002a918c3c
CodeMouse179 <codemouse179@gmail.com>
2023-04-10 23:44:14 +0300

    add first impl for Windows

--- doc/README.md
+++ doc/README.md
@@ -1,4 +1,6 @@
 # doc
+## agreement
+1. All source code must be encoded in UTF-8 or UTF-8 with BOM.
 ## tools
 1. CMake 3.26.3
 1. Github Desktop 3.2.1
--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#if defined(_WIN32)
+﻿#if defined(_WIN32)
 #define SYSTEM_WINDOWS 1
 #endif
 #if defined(__linux__)
@@ -10,7 +10,61 @@
 #if defined(SYSTEM_LINUX) || defined(SYSTEM_MACOS)
 #define SYSTEM_POSIX 1
 #endif
-int main()
+#if defined(_MSC_VER)
+#define MICROSOFT_COMPILER 1
+#endif
+
+#define true 1
+#define false 0
+typedef int bool;
+
+bool init();
+bool deinit();
+int str_len(const char* str);
+void print(const char* str);
+
+#if defined(MICROSOFT_COMPILER)
+#pragma execution_character_set("utf-8")
+#endif
+
+#if defined(SYSTEM_WINDOWS)
+#include <Windows.h>
+int console_output_cp = 0;
+bool init()
 {
+    console_output_cp = GetConsoleOutputCP();
+    if (console_output_cp == 0) return false;
+    BOOL ret = SetConsoleOutputCP(CP_UTF8);
+    return ret;
+}
+bool deinit()
+{
+    BOOL ret = SetConsoleOutputCP(console_output_cp);
+    return ret;
+}
+int str_len(const char* str)
+{
+    int length = 0;
+    while (*str)
+    {
+        length++;
+        str++;
+    }
+    return length;
+}
+void print(const char* str)
+{
+    HANDLE std_output_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (std_output_handle == INVALID_HANDLE_VALUE || std_output_handle == NULL) return;
+    DWORD written;
+    WriteConsoleA(std_output_handle, str, str_len(str), &written, NULL);
+}
+#endif
+
+int main(int argc, char** argv)
+{
+    init();
+    print("你好，世界😄hello, world\n");
+    deinit();
     return 0;
 }
